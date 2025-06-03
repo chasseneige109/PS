34a6a948d3a6803d7a0f608a30d32b26a1f12bc6
chasseneige109 <liszt10917@gmail.com>
2025-06-03 19:58:05 +0900

    반복문

---
+++ ProbSol/반복문/코딩은체육과목입니다.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	int length = N / 4;
+
+	for (int i = 0; i < length; i++)
+	{
+		cout << "long ";
+	}
+	cout << "int" << endl;
+
+	return 0;
+
+}
